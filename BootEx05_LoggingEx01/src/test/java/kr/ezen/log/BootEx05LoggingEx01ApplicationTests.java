aa65c7326ddd8ff59d028861ea4e543c5ebb9a59
kimc <ezenacademy202310@gmail.com>
2024-01-15 15:41:48 +0900

    commit

---
+++ BootEx05_LoggingEx01/src/test/java/kr/ezen/log/BootEx05LoggingEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ezen.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootEx05LoggingEx01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
